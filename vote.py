9c2a1c5f97d7087ac8e79961af2bcd4761ac8fbf
Aditya Singh Yadav <151426869+theAdityadav@users.noreply.github.com>
2024-08-30 18:30:18 +0530

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,7 @@
+age=int(input("enter your age: "))
+if(age>=18):
+    print("your are eligible for vote:)")
+elif(age>=15 and age<=18):
+    print("you are supposed to gay:)")
+else:
+    print("why are you gay")
