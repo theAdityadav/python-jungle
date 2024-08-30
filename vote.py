36dcd0d3af3bae18702729a53191138bc75ff150
Aditya Singh Yadav <151426869+theAdityadav@users.noreply.github.com>
2024-08-30 18:51:01 +0530

    vote.py

--- vote.py
+++ vote.py
@@ -1,7 +1,9 @@
-age=int(input("enter your age: "))
-if(age>=18):
-    print("your are eligible for vote:)")
-elif(age>=15 and age<=18):
-    print("you are supposed to gay:)")
+age=int(input("enter your age: ")) #enter the age from the user
+calc=18-age #check the age gap
+if age<0:
+    print("inavlid input") #invalid in negative number
+elif age<18:
+    Pyear=int(input("enter the present year: ")) #ask only if the age is less than 18 and to check the voting year
+    print(f"your are not eligible for vote after {calc} years in {Pyear+calc} you'r eligible for vote")
 else:
-    print("why are you gay")
+    print("you are eligible for vote")
